767ca1c6bd0b6ced25c31f70df6b9e67268d90b0
Eitan Wechsler <wexeitan@gmail.com>
2021-12-22 18:31:53 +0200

    wip

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,3 +1,16 @@
-import GraphAlgoInterface
+from src.GraphAlgoInterface import GraphAlgoInterface
+
+from GRF_Node import GRF_Node
 
 class GraphAlgo(GraphAlgoInterface):
+    def load_from_json(self, file_name: str) -> bool:
+        pass
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        pass
+
+    def plot_graph(self) -> None:
+        pass
